9430eda3addbdfcf6db3475a2137907bfcb8846c
sampadapokale <sampada.pokale@omfysgroup.com>
2024-08-29 12:28:16 +0530

    Add files via upload

---
+++ src/pages/Card1.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import Cards from './Cards';
+import Menu from './Menu';
+import  Header  from './Header';
+const Card1 = () => {
+  return (
+    <div>
+      <Header></Header>
+      <Menu />
+      <Cards />
+      
+    </div>
+  )
+}
+
+export default Card1
